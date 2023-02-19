ad40952a88603348b491730983aa3fc02eb5b93d
GMaidana95 <102345406+GMaidana95@users.noreply.github.com>
2023-02-18 21:09:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+ScrollReveal().reveal('.nombre-inicio', { delay: 500 })
+ScrollReveal().reveal('.profesion-inicio', { delay: 2000 })
+ScrollReveal().reveal('.sobre-mi', {delay: 250});
+ScrollReveal().reveal('.experiencia', {delay: 250});
+ScrollReveal().reveal('.estudios', {delay: 250});
